68e8f7c3bb6c3e2af9a6693719b6d758fea9951b
woodok <newwoodok@gmail.com>
2015-12-15 09:57:48 +0900

    (D) p3 tested.

--- ProjectEuler/p3_main.cpp
+++ ProjectEuler/p3_main.cpp
@@ -5,7 +5,7 @@ int main()
 	int testvalue = 10;
 	std::cout << "program start" << std::endl;
 
-	std::cout << findLargestPrimeFactor(10) << std::endl;
+	std::cout << findLargestPrimeFactor(51) << std::endl;
 
 	return 0;
 }
