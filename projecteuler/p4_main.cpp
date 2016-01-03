10489d4b5101774d5250660e395bf32b18c74c6e
woodok <newwoodok@gmail.com>
2016-01-03 21:57:47 +0900

    D: wrap up save.

--- projecteuler/p4_main.cpp
+++ projecteuler/p4_main.cpp
@@ -1,37 +1,49 @@
 #include "p4_header.h"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// test code
-int main()
+int main(void)
 {
-	//// case1
-	//std::cout << checkDigits(989, 3) << std::endl;
-	//std::cout << checkDigits(99, 3) << std::endl;
-	//std::cout << checkDigits(111, 0) << std::endl;
+	int x;
+	
+	// Palindrome generator in descending order 
+	auto palindrome = [] (int _digits) mutable -> bool {
+		int retVal;
+		int 
+	};
 
-	//// case2
-	//std::cout << getPairFactor(100, 20) << std::endl;
-	//std::cout << getPairFactor(100, 3) << std::endl;
-
-	// case3
-	std::cout << checkNumber(100, 2) << std::endl;
-	std::cout << checkNumber(100, 3) << std::endl;
-
-	std::cout << "Program terminated." << std::endl;
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////
+//// test code
+//int main()
+//{
+//	//// case1
+//	//std::cout << checkDigits(989, 3) << std::endl;
+//	//std::cout << checkDigits(99, 3) << std::endl;
+//	//std::cout << checkDigits(111, 0) << std::endl;
+//
+//	//// case2
+//	//std::cout << getPairFactor(100, 20) << std::endl;
+//	//std::cout << getPairFactor(100, 3) << std::endl;
+//
+//	// case3
+//	std::cout << checkNumber(100, 2) << std::endl;
+//	std::cout << checkNumber(100, 3) << std::endl;
+//
+//	std::cout << "Program terminated." << std::endl;
+//}
